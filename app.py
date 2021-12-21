60a056de79fd7c0fa0fd8b4c9214425bf88c592d
Matthias Bussonnier <bussonniermatthias@gmail.com>
2021-12-21 21:10:41 +0100

    print headers

--- app.py
+++ app.py
@@ -18,11 +18,12 @@ def test():
 
 @app.route('/email', methods=['POST'])
 def receive_email():
-    print('From:', request.form['from'])
-    print('To:', request.form['to'])
-    print('Subject:', request.form['subject'])
-    print('Body:', request.form['text'])
-    return ''
+    print("Headers:", request.form["headers"])
+    print("From:", request.form["from"])
+    print("To:", request.form["to"])
+    print("Subject:", request.form["subject"])
+    print("Body:", request.form["text"])
+    return ""
 
 
 if __name__ == "__main__":
